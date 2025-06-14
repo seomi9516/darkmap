f62044efbdb9d584f79700ba1abe5629c699b586
seomi9516 <seomi9516@gmail.com>
2025-06-14 19:49:18 +0900

    fix: TestMap에 ControlPopup이 포함되어 있으므로 제거

--- src/components/RenderMap.vue
+++ src/components/RenderMap.vue
@@ -2,13 +2,11 @@
   <div class="render-map">
     <TestMap v-if="!isMobile" />
     <ControlMobile v-if="isMobile" />
-    <ControlPopup v-else />
   </div>
 </template>
 
 <script setup>
 import TestMap from '@/components/TestMap.vue';
-import ControlPopup from '@/components/ControlPopup.vue';
 import ControlMobile from '@/components/ControlMobile.vue';
 import { useDevice } from '@/composables/useDevice';
 
